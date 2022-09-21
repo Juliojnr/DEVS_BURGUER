8fa6667f7ebbe56aa220861ec6d1862431229c17
Anttonino <victor.antonino12@gmail.com>
2022-09-21 11:17:16 -0300

    update: Script, HTML and CSS
    Código de script atualizado para a estrutura de classes e heranças e pagina de marcação e estilização atualizadas

--- src/script.ts
+++ src/script.ts
@@ -1,41 +1,31 @@
-class menu {
-	private value: number;
-	
-	constructor(value: number){
-		this.value = value;
-    }
-    
-    public getValue (): number {
-       return this.value
-    }
+class Product  {
+   private readonly product_name: string
+   private readonly value: number;
+
+   constructor (product_name: string, value: number) {
+       this.product_name = product_name;
+       this.value = value;
+   }
 }
 
-let value = 0
+class Burguer extends Product {
+   igredients: string;
+   vegan: boolean;
 
-const Menu = new menu (value);
-
-function burger1 () { 
-    value = value + 23
-    alert('Valor adicionado');
- }
-
- function burger2 () { 
-    value += 15
-    alert('Valor adicionado');
- }
-
- function burger3 () { 
-    value += 50
-    alert('Valor adicionado');
- }
-
- function burger4 () { 
-    value += 70
-    alert('Valor adicionado');
- }
-
-function carrinho () {
-    document.write('<h1>VALOR FINAL</h1>')
-    document.write(`Valor final: R$${value}`)
+   constructor (product_name: string, value: number, igredients: string, vegan:boolean) {
+       super(product_name,value);
+       this.igredients = igredients;
+       this.vegan = vegan;
+   }
 }
 
+class Drinks extends Product {
+   type: string;
+   alcoholic: boolean;
+
+   constructor (product_name: string, value: number, type: string, alcoholic:boolean) {
+       super(product_name,value);
+       this.type = type;
+       this.alcoholic = alcoholic;
+   }
+}
