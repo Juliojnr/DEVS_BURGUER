cf02641dba43bd636fc6c6b10ec57ce5fe689bae
Anttonino <victor.antonino12@gmail.com>
2022-09-22 18:12:05 -0300

    feat: Exibição dos objetos no script

--- src/script.ts
+++ src/script.ts
@@ -32,69 +32,77 @@ class Drinks extends Product {
 
 class Client {
     public username: string;
-    public endereco: string;
+    public address: string;
 
-    constructor (username: string, endereco: string){
+    constructor (username: string, address: string){
         this.username = username;
-        this.endereco = endereco;
+        this.address = address;
     }
 }
 
 let final_value = 0
 const products: Product[] = []
+const burguer: Burguer[] = []
+
+let isVegan: false
 
 
-let xegg = new Burguer('X-Egg',5,'Pão brioche, hambúrguer, alface, queijo, molho',false);
-let xbacon = new Burguer('X-Bacon', 18,'Pão brioche, hambúrguer, alface, bacon, queijo, presunto, molho, batata palha', false);
-let xduplo = new Burguer('X-Duplo', 9,'Pão brioche,2 hambúrguer, alface, queijo, presunto, molho',false);
-let xlarica = new Burguer('X-Larica', 15, 'Pão brioche, 2 hambúrguer, alface, tomate,2 bacon,2 queijo,2 presunto, molho da casa, batata palha',false);
-let xsalada = new Burguer('X-salada', 23,'Pão de gergelim, hambúrguer feito de feijão servido, alface , tomate, ketchup e maionese isVegana',false)
-let xvegan = new Burguer('X-Vegan', 23,'Pão de gergelim, hambúrguer feito de feijão servido, alface , tomate, ketchup e maionese isVegana',true)
+let xegg= new Burguer('X-Egg',15.99,'1 Carne, ovo, queijo',false);
+let xbacon = new Burguer('X-Bacon', 17.99,'1 Carne, bacon, 1 queijo', false);
+let xlarica = new Burguer('X-Larica', 19.90, '2 Carnes, 2 queijos, salada, gergelim',false);
+let xsalada = new Burguer('X-Salada', 16.99, '1 Carne, queijo, bacon, salada, gergelim',false);
+let xduplo = new Burguer('X-Duplo', 18.99,'2 Carnes, 2 queijos, picles, gergelim',false);
+let xvegan = new Burguer('X-Vegan', 22.99,'Carne de lentilha, cebola, cheiro verde',true)
 
 function Fxegg () {
     alert('Produto adicionado ao carrinho');
     final_value += xegg.value;
     products.push(xegg);
+    burguer.push(xegg);
 }
 
 function Fxbacon () {
     alert('Produto adicionado ao carrinho')
     final_value += xbacon.value
     products.push(xbacon)
+    burguer.push(xbacon);
 }
 
 function Fxduplo () {
     alert('Produto adicionado ao carrinho')
     final_value += xduplo.value
     products.push(xduplo)
+    burguer.push(xduplo)
 }
 
 function Fxlarica () {
     alert('Produto adicionado ao carrinho')
     final_value += xlarica.value
     products.push(xlarica)
+    burguer.push(xlarica)
 }
 
 function Fxsalada () {
     alert('Produto adicionado ao carrinho')
     final_value += xsalada.value
     products.push(xsalada)
-}
-
-function Fxvegan () {
-    alert('Produto adicionado ao carrinho')
-    final_value += xvegan.value
-    products.push(xvegan)
+    burguer.push(xsalada)
 }
 
 function finalizePurchase () {
-    document.write('<h1>Finalize seu pedido</h1>')
+    document.write('<h1>Pedido Finalizado</h1>')
+    document.write('<h3>O site <DEVS BURGUER> está passando por algumas manutençõeso</h3>')
+    document.write('<h3>Aguardem as novidades ( ͡° ͜ʖ ͡°)</h3>')
     const username = prompt('Digite seu nome')
     const address = prompt('Digite seu endereço')
     let client = new Client (username, address)
-    document.write(`<h3>valor R$${final_value}</h3>`)
-    document.write(`<h3>valor R$${client}</h3>`)
+    document.write(`<h3>Obrigado ${client.username} seu pedido já será entregue para ${client.address}</h3>`)
+    document.write(`<h3>valor final do pedido R$${final_value.toFixed(2)}</h3>`)
+
     for (let index = 0; index < products.length; index++) {
-        document.write(`${products}`)
+        document.write (`<h3><br>Seu pedido</br></h3>`)
+        document.write (`Hambúrguer ${products[index].product_name}`)
+        document.write (`Valor R$${products[index].value.toFixed(2)}`)
+        document.write (`Igredientes ${burguer[index].igredients}`)
     }
 }
